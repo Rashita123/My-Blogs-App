2f673ea7d529535fbe9fe2c6cf6671468c69ab32
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-09 13:21:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Check out my Blogs</h1>
+      <h2>Click on any niche to see my blogs</h2>
+      <button>JavaScript Basics</button>
+      <button>Cool JavaScript</button>
+      <button>Random Blogs</button>
+    </div>
+  );
+}
