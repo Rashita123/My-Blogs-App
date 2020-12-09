98a8e5596c9541f4cd2b7789bf6226cc7b36601d
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2020-12-09 13:55:05 +0530

    Blogs topics done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
+const topicDictionary = {
+  "JavaScript Basics": ["Leson-1", "Leson-2"],
+  "Cool JavaScript": ["JavaScript for LinkedIn"],
+  "Random Blogs": ["College Nostalgia", "Poetry"],
+  "": ""
+};
+const list = ["JavaScript Basics", "Cool JavaScript", "Random Blogs"];
 export default function App() {
+  const [lis, setlist] = useState([]);
+  function getBlogs(topic) {
+    return topicDictionary[topic];
+  }
+  function clickHandler(topic) {
+    setlist(getBlogs(topic));
+  }
   return (
     <div className="App">
       <h1>Check out my Blogs</h1>
       <h2>Click on any niche to see my blogs</h2>
-      <button>JavaScript Basics</button>
-      <button>Cool JavaScript</button>
-      <button>Random Blogs</button>
+
+      {list.map((topic) => {
+        return <button onClick={() => clickHandler(topic)}>{topic}</button>;
+      })}
+      <ul>
+        {lis.map((blog) => {
+          return <li>{blog}</li>;
+        })}
+      </ul>
     </div>
   );
 }
